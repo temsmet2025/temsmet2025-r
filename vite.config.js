c100c5c7fb1ffa0a00cd3263c19e52cb0ba26f28
parvej1999 <parvej@debian.debian>
2024-10-30 16:16:59 +0530

    Deploed

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "https://github.com/parvej1999/temsmet.github.io.git",
+  base: "/temsmet.github.io/",
   plugins: [react()],
 })
