5b2ea4bafdd309aae97fd3ceaa9a91f4d3c282d5
parvej1999 <parvej@debian.debian>
2024-10-31 22:34:01 +0530

    converted major part to react

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/temsmet.github.io",
+  // base: "/temsmet.github.io",
+  base: "temsmet2025-r",   //when pushing on temsmet
   plugins: [react()],
 })
