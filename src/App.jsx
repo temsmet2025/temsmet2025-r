f3ecb0fa2c40f1b7c14d50bac02955ff627d1f87
Pradneya <pradneya.prog@gmail.com>
2024-11-11 22:20:39 +0530

    Made the navbar and venue page responsive. Also fixed the Tailwind bug

--- src/App.jsx
+++ src/App.jsx
@@ -74,12 +74,12 @@ function App() {
    * Uncomment the line below for the appropriate deployment path.
    */
   // { basename: "/temsmet.github.io" }, // for GitHub Pages deployment
-  { basename: "/temsmet2025-r" } // for GitHub Pages deployment
+  // { basename: "/temsmet2025-r" } // for GitHub Pages deployment
   )
 
   return (
     <>
-      <RouterProvider router={router} />
+      <RouterProvider router={router} />  
     </>
   )
 }
