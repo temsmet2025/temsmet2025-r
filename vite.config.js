b88fa93aa9ab7f70fd59a5b9d3702c68ceb6d1f3
parvej khan <1999monustp@gmail.com>
2024-11-12 10:25:41 +0530

    making live

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   // base: "/temsmet.github.io",
-  // base: "/temsmet2025-r",   //when pushing on temsmet and also change in router in app.jsx
+  base: "/temsmet2025-r",   //when pushing on temsmet and also change in router in app.jsx
   base:"",
   build: {
     outDir: 'dist', // Ensure this is set to 'dist' (the default build output folder)
