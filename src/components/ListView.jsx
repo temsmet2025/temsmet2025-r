ba0d0ce02e8b38485b36e46fa5abe9f2cd2afce4
Pradneya <pradneya.prog@gmail.com>
2024-11-12 23:09:49 +0530

    Updated the mobile responsiveness of Call for Paper page

--- src/components/ListView.jsx
+++ src/components/ListView.jsx
@@ -1,24 +1,28 @@
 // used for call-for-papers
 function ListView(props) {
-    const dataItem = props.data;
-    return (
-        <div id={`props.title`} className="ml-32 mr-32 mt-10 mb-10">
-            <h1 className="font-bold text-4xl ">{props.title}</h1>
-            {dataItem.map((item, index) => (
-                <span key={index} id={`tracks-${index}`}>
-                    <h1 className="font-bold mt-7  text-lg">{item.track}</h1>
-                    <ul className="list-disc ml-10">
-                    {item.topics.map((topic, index) => (
-                        <li key={index} className="mt-1">
-                            {topic}
-                        </li>
-                    ))}
-                    </ul>
-                </span>
-            ))}
-            {/* {console.log(dataItem)} */}
-            
-        </div>
-    )
+  const dataItem = props.data;
+  return (
+    <div
+      id={`props.title`}
+      className="flex flex-col justify-center items-center mt-8 mb-8"
+    >
+      <div>
+        <h1 className="font-bold text-4xl text-center">{props.title}</h1>
+        {dataItem.map((item, index) => (
+          <div key={index} id={`tracks-${index}`} className="p-8">
+            <h1 className="font-bold mt-7 text-lg">{item.track}</h1>
+            <ul className="list-disc ml-10">
+              {item.topics.map((topic, index) => (
+                <li key={index} className="mt-1">
+                  {topic}
+                </li>
+              ))}
+            </ul>
+          </div>
+        ))}
+        {/* {console.log(dataItem)} */}
+      </div>
+    </div>
+  );
 }
-export default ListView
+export default ListView;
