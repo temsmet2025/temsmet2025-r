105fdda7c7c91ab6abe393b90a08ecb4a9c6c777
Pradneya <pradneya.prog@gmail.com>
2024-11-12 23:18:53 +0530

    Updated the mobile responsiveness of Call for Paper page

--- src/components/ListView.jsx
+++ src/components/ListView.jsx
@@ -7,10 +7,10 @@ function ListView(props) {
       className="flex flex-col justify-center items-center mt-8 mb-8"
     >
       <div>
-        <h1 className="font-bold text-4xl text-center">{props.title}</h1>
+        <h1 className="font-bold text-4xl text-center pb-4">{props.title}</h1>
         {dataItem.map((item, index) => (
-          <div key={index} id={`tracks-${index}`} className="p-8">
-            <h1 className="font-bold mt-7 text-lg">{item.track}</h1>
+          <div key={index} id={`tracks-${index}`} className="p-4">
+            <h1 className="font-bold text-lg">{item.track}</h1>
             <ul className="list-disc ml-10">
               {item.topics.map((topic, index) => (
                 <li key={index} className="mt-1">
