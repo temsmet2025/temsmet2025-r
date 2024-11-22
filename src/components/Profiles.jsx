28f39603e6de7d5c887b3f104a297ffa642fdfd9
parvej khan <1999monustp@gmail.com>
2024-11-22 19:22:04 +0530

    updated images of Sudeendra, Indu joshi, pallavi athe

--- src/components/Profiles.jsx
+++ src/components/Profiles.jsx
@@ -5,6 +5,7 @@ const Profiles = (props) => {
   return (
     <div className="w-full  sm:m-10">
           <h1 className="sm:text-3xl text-xl font-semibold mt-5 ml-2 sm:mt-10">{props.title}</h1>
+          <div class="mt-2 mb-5 h-0.5 bg-gray-950 w-10/12"></div>
           <div id={`profiles-${props.title}`} className='flex flex-wrap justify-center sm:justify-normal' >
               {data.map((profile, index)=>(
                   <div key={index} className="card relative w-64 sm:w-80 h-96 lg:h-52 bg-slate-50 rounded-md border hover:bg-slate-300/80 m-1 sm:m-5">
