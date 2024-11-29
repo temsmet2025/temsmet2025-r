3333e0818a40aa6655d912516015345ffb0bbd7f
Pradneya <pradneya.prog@gmail.com>
2024-11-29 20:59:26 +0530

    Updated the venue page and pre nav component

--- src/components/Navbar/menuItems.js
+++ src/components/Navbar/menuItems.js
@@ -1,7 +1,7 @@
 /**
  * Array representing the main navigation items for the website.
  * Each object contains the `name` of the navigation link, `href` (URL the link points to), and `current` (indicates if the link is active).
- * 
+ *
  * @constant
  * @type {Array<Object>}
  * @property {string} name - The name of the navigation link.
@@ -9,20 +9,24 @@
  * @property {boolean} current - A boolean indicating if the link is currently active.
  */
 export const navigation = [
-  { name: 'Home', href: '/', current: "_self" },
-  { name: 'Call for Papers', href: '#call-for-papers', current: "_self" },
-  { name: 'Submission Guidelines', href: '/submission-guidelines', current: "_self" },
-  { name: 'Registration', href: '/registration', current: "_self" },
-  { name: 'Venue And Dates', href: '/venue', current: "_self" },
-  { name: 'Organising Committee', href: '/org-committee', current: "_self" },
-  { name: 'Past Editions', href: '/', current: "_self" },
-  { name: 'Contact', href: '/', current: "_self" },
+  { name: "Home", href: "/", current: "_self" },
+  { name: "Call for Papers", href: "#call-for-papers", current: "_self" },
+  {
+    name: "Submission Guidelines",
+    href: "/submission-guidelines#submission-guidelines",
+    current: "_self",
+  },
+  { name: "Registration", href: "/registration", current: "_self" },
+  { name: "Venue And Dates", href: "/venue", current: "_self" },
+  { name: "Organising Committee", href: "/org-committee", current: "_self" },
+  { name: "Past Editions", href: "/", current: "_self" },
+  { name: "Contact", href: "/", current: "_self" },
 ];
 
 /**
  * Array representing the submenu items under the 'About' section.
  * Each object contains the `name` of the submenu item, `href` (URL the item points to), and `current` (indicates if the item is active).
- * 
+ *
  * @constant
  * @type {Array<Object>}
  * @property {string} name - The name of the submenu item.
@@ -39,7 +43,7 @@ export const aboutSubmenuItems = [
 /**
  * Array representing the submenu items under the 'Call for Papers' section.
  * Each object contains the `name` of the submenu item, `href` (URL the item points to), and `current` (indicates if the item is active).
- * 
+ *
  * @constant
  * @type {Array<Object>}
  * @property {string} name - The name of the submenu item.
@@ -48,14 +52,18 @@ export const aboutSubmenuItems = [
  */
 export const cfpSubmenuItems = [
   { name: "Conference Tracks", href: "/call-for-papers", current: "_self" },
-  { name: "Paper Preparation & Submission Guidelines for Authors", href: "/", current: "_self" },
+  {
+    name: "Paper Preparation & Submission Guidelines for Authors",
+    href: "/",
+    current: "_self",
+  },
   { name: "Important Dates", href: "/", current: "_self" },
 ];
 
 /**
  * Array representing the submenu items under the 'Past Editions' section.
  * Each object contains the `name` of the past edition, `href` (URL the item points to), and `current` (indicates if the item is active).
- * 
+ *
  * @constant
  * @type {Array<Object>}
  * @property {string} name - The name of the past edition.
@@ -63,7 +71,19 @@ export const cfpSubmenuItems = [
  * @property {boolean} current - A boolean indicating if the past edition item is currently active.
  */
 export const pastEditionsItems = [
-  { name: "2020", href: "https://ieeexplore.ieee.org/xpl/conhome/9557352/proceeding", current: "_blank" },
-  { name: "2021", href: "https://ieeexplore.ieee.org/xpl/conhome/9768306/proceeding", current: "_blank" },
-  { name: "2023", href: "https://ieeexplore.ieee.org/xpl/conhome/10149893/proceeding", current: "_blank" },
+  {
+    name: "2020",
+    href: "https://ieeexplore.ieee.org/xpl/conhome/9557352/proceeding",
+    current: "_blank",
+  },
+  {
+    name: "2021",
+    href: "https://ieeexplore.ieee.org/xpl/conhome/9768306/proceeding",
+    current: "_blank",
+  },
+  {
+    name: "2023",
+    href: "https://ieeexplore.ieee.org/xpl/conhome/10149893/proceeding",
+    current: "_blank",
+  },
 ];
--- src/components/PreNav.jsx
+++ src/components/PreNav.jsx
@@ -1,17 +1,40 @@
+import { useState, useEffect } from 'react';
+
 
 function PreNav() {
+    function useIsDesktop() {
+        const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+        useEffect(() => {
+            function handleResize() {
+                setWindowWidth(window.innerWidth);
+            }
+
+            window.addEventListener('resize', handleResize);
+            return () => window.removeEventListener('resize', handleResize);
+        }, []);
+
+        return windowWidth;
+    }
+
+    const windowWidth = useIsDesktop();
+    const isDesktop = windowWidth >= 1024;
+    const isTablet = windowWidth >= 768 && windowWidth < 1024;
+
     return (
-        <section id="pre-nav" className="h-52 w-full flex justify-between items-center p-10">
-            <div id="nit-logo" className="h-[10rem] w-[10rem]">
-                <img src="./logos/nit-logo/NIT-Delhi_Logo.png" className="h-[10rem] w-[10rem]" alt="NIT-DELHI-LOGO" />
+        <section id="pre-nav" className="h-fit w-full flex justify-between items-center p-4">
+            <div id="nit-logo" className="aspect-square flex justify-center" style={{ width: isDesktop ? '8rem' : isTablet ? '6rem' : '4rem' }}>
+                <img src="./logos/nit-logo/NIT-Delhi_Logo.png" className="aspect-square" style={{ width: isDesktop ? '8rem' : isTablet ? '6rem' : '4rem' }} alt="NIT-DELHI-LOGO" />
             </div>
-            <div id="tems-logos" className="h-[8rem] w-[25rem]">
-                <div id="tems-management">
-                    <img src="./logos/IEEE TEMS logo/17-TA-213 TEMS logo RGB_bg_removed.png" className="h-[8rem] w-[25rem]" alt="TEMS-LOGO" />
+            <div id="tems-logos" className="flex justify-center" style={{ width: isDesktop ? '20rem' : isTablet ? '12rem' : '8rem' }}>
+                <div id="tems-management" className='flex justify-center'>
+                    {isDesktop && <img src="./logos/IEEE TEMS logo/17-TA-213 TEMS logo RGB_bg_removed.png" className="w-full" alt="TEMS-LOGO" />}
+                    {isTablet && !isDesktop && <img src="./logos/IEEE TEMS logo/17-TA-213 TEMS logo mobile.jpeg" className="w-full" alt="TEMS-LOGO" />}
+                    {!isDesktop && !isTablet && <img src="./logos/IEEE TEMS logo/17-TA-213 TEMS logo mobile.jpeg" className="w-full" alt="TEMS-LOGO" />}
                 </div>
             </div>
-            <div id="ieee-logo" className="h-[8rem] w-[20rem]">
-                <img src="./logos/IEEE-logo/IEEE_logo.png" alt="IEEE-LOGO" className="h-[8rem] w-[20rem]"/>
+            <div id="ieee-logo" className="flex justify-center" style={{ width: isDesktop ? '15rem' : isTablet ? '10rem' : '6rem' }}>
+                <img src="./logos/IEEE-logo/IEEE_logo.svg" alt="IEEE-LOGO" className="w-full"/> 
             </div>
         </section>
     )
