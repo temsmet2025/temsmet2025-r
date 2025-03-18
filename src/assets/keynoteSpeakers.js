c2d63f3ce6e700a16ee9a25d79aef713391dc0d7
Parvej Khan <1999monustp@gmail.com>
2025-03-18 13:52:32 +0530

    added dr menon as keynote speaker

--- src/assets/keynoteSpeakers.js
+++ src/assets/keynoteSpeakers.js
@@ -1,4 +1,5 @@
 export const keynoteSpeakers = [
     { id: 0, name: "Sachin Sinha" , affiliation: "Microsoft", imagePath: "./keynoteSpeakers/sachinsinha.jpeg" },
-    { id: 1, name: "Mayank Sawhney" , affiliation: "Atlassian   ", imagePath: "./keynoteSpeakers/mayank.jpeg" }
+    { id: 1, name: "Mayank Sawhney" , affiliation: "Atlassian", imagePath: "./keynoteSpeakers/mayank.jpeg" },
+    { id: 2, name: "Dr K Rajalakshmi Menon", affiliation: "FNAE, Distinguished Scientist & Director General(Aeronautical Systems) DRDO", imagePath: "./keynoteSpeakers/mayank.jpeg", link:"https://waves2024.pmikerala.org/speaker/dr-k-rajalakshmi-menon/" }
 ];
--- vite.config.js
+++ vite.config.js
@@ -19,8 +19,8 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   // base: "/temsmet2025-r",
   build: {
-    // outDir: 'dist',
-    outDir: '../temsmet builds/liveBuild-v3.7',
+    outDir: 'dist',
+    // outDir: '../temsmet builds/liveBuild-v3.9',
     rollupOptions: {
       output: {
         manualChunks(id) {
