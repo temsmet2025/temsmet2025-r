dfba8fde51a9f206ad2c9b5a8385214a2ac32e39
Parvej Khan <1999monustp@gmail.com>
2025-03-31 21:52:45 +0530

    added the footer and some changes in speakers

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,34 +1,61 @@
 import { FaFacebookF, FaInstagram, FaLinkedin } from "react-icons/fa";
-
+import { Link } from "react-router-dom";
+const scrollToSection = (id) => {
+  const timer = setTimeout(() => {
+    const element = document.getElementById(id);
+    if (element) {
+      const rect = element.getBoundingClientRect();
+      window.scroll({
+        top: window.scrollY + rect.top,
+        behavior: "smooth",
+      });
+    }
+  }, 0);
+};
 const Footer = () => {
   return (
-    <footer className="bg-black text-white p-10">
-      <div className="max-w-7xl mx-auto flex flex-col md:flex-row justify-between">
-        
-        <div className="w-1/2 text-left">
-          <nav className="flex flex-col space-y-2">
-            <a href="#" className="hover:text-gray-400">Track1 - Innovations in Power Engineering</a>
-            <a href="#" className="hover:text-gray-400">Track2 - AI, ML and Cyber Security</a>
-            <a href="#" className="hover:text-gray-400">Track3 - Smart Systems and Communication</a>
-            <a href="#" className="hover:text-gray-400">Track4 - Engineering and Smart Management</a>
-            <a href="#" className="hover:text-gray-400">Track5 - Advancements in VLSI, Embedded systems and Signal Processing</a>
-            <a href="#" className="hover:text-gray-400">Track6 - Innovations and Entrepreneurship</a>
-          </nav>
-        </div>
+    <footer className=" bg-black text-white p-10 w-[100vw]">
+        <div className="flex justify-center items-center">
+            <div className="grid grid-flow-row grid-cols-4 gap-10 md:flex-row justify-between w-full">
+            
+                <div className="text-left col-span-1">
+                    <nav className="flex flex-col gap-3 text-base text-gray-400">
+                        <Link to="/call-for-papers" className="hover:text-white">Track1 - Innovations in Power Engineering</Link>
+                        <Link to="/call-for-papers" className="hover:text-white">Track2 - AI, ML and Cyber Security</Link>
+                        <Link to="/call-for-papers" className="hover:text-white">Track3 - Smart Systems and Communication</Link>
+                        <Link to="/call-for-papers" className="hover:text-white">Track4 - Engineering and Smart Management</Link>
+                        <Link to="/call-for-papers" className="hover:text-white">Track5 - Advancements in VLSI, Embedded systems and Signal Processing</Link>
+                        <Link to="/call-for-papers" className="hover:text-white">Track6 - Innovations and Entrepreneurship</Link>
+                    </nav>
+                </div>
 
-        <div className="w-1/2 text-right">
-          <h2 className="text-lg font-bold">NEW DELHI</h2>
-          <p>Zone P1, GT Karnal Road</p>
-          <p>Bakoli, 110036</p>
-          <p>+11-33861122/83</p>
-          <p>temsmet2025@nitdelhi.ac.in</p>
-          <div className="flex justify-end space-x-4 mt-4">
-            <a href="https://www.facebook.com" className="text-white hover:text-gray-400"><FaFacebookF /></a>
-            <a href="https:www.instagram.com" className="text-white hover:text-gray-400"><FaInstagram /></a>
-            <a href="https://www.linkedin.com/company/temsmet2025-nitdelhi" className="text-white hover:text-gray-400"><FaLinkedin /></a>
-          </div>
+                <div className="flex flex-col items-center justify-center col-span-2">
+                      <h1 className="text-6xl font-bold">TESMET 2025</h1>
+                      <div className="flex flex-row text-lg gap-2 text-gray-400 font-semibold">
+                          <Link to="/submission-guidelines" className="hover:text-white">Submission Guidelines  </Link> 
+                          <span>|</span>
+                          <Link to="/" onClick={(e) => {
+                              scrollToSection("Important Dates");
+                          }} className="hover:text-white">Important Dates</Link> 
+                          <span>|</span>
+                          <Link to="/venue&directions" className="hover:text-white">Venue & Direction</Link>
+                          <span>|</span>
+                        <Link to="/explore-delhi" className="hover:text-white">Explore Delhi</Link>
+                      </div>
+                </div>
+
+                <div className="ml-10 flex flex-col ">
+                    <h2 className="text-lg font-bold">NEW DELHI</h2>
+                      <div className="text-gray-400 flex flex-col ">
+                          <span>Zone P1, GT Karnal Road</span>
+                    <span>Bakoli, 110036</span>
+                    <span>+11-33861122/83</span>
+                    <span>temsmet2025@nitdelhi.ac.in</span>
+                    </div>
+                
+                </div>
+            </div>
         </div>
-      </div>
 
       <div className="text-center mt-10 text-gray-400">
         ©2025 IEEE - All Rights Reserved
