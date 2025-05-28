40a3972615a190d42ab21f93a26f16e2e4e10212
parvej.khan <parvej.khan@collegedekho.com>
2025-05-28 10:58:08 +0530

    commented abhay and divyesh from speakers

--- src/assets/keynoteSpeakers.js
+++ src/assets/keynoteSpeakers.js
@@ -5,8 +5,8 @@ export const keynoteSpeakers = [
     { id: 3, name: "Sachin Sinha" , affiliation: "Director, Technology Strategy, Microsoft", imagePath: "./keynoteSpeakers/sachinsinha.webp", link:"https://www.linkedin.com/in/sachinsinha/" },
     { id: 4, name: "Mayank Sawhney" , affiliation: "Senior Engineering Manager, Atlassian", imagePath: "./keynoteSpeakers/mayank.webp", link:"https://www.linkedin.com/in/mayanksawhney/?originalSubdomain=in" },
     { id: 5, name: "Sandip Patil" , affiliation: "Entrepreneur E-Spin Nanotech", imagePath: "./keynoteSpeakers/sandip.jpeg", link:"https://www.linkedin.com/in/dr-sandip-patil-87863814/?originalSubdomain=in" },
-    { id: 6, name: "Abhay Gupta", affiliation: "Efficient World, Vancouver, Canada (Chair)", imagePath: "./orgCommittee/informationCommittee/abhay.webp" },
-    { id: 7, name: "Divyesh Dixit", affiliation: "Co-Founder BambooBox", imagePath: "./orgCommittee/techProgCommittee/divyesh.webp", link: "https://www.linkedin.com/in/divyesh-dixit/" },
+    // { id: 6, name: "Abhay Gupta", affiliation: "Efficient World, Vancouver, Canada (Chair)", imagePath: "./orgCommittee/informationCommittee/abhay.webp" },
+    // { id: 7, name: "Divyesh Dixit", affiliation: "Co-Founder BambooBox", imagePath: "./orgCommittee/techProgCommittee/divyesh.webp", link: "https://www.linkedin.com/in/divyesh-dixit/" },
     { id: 8, name: "Andrea Tagarelli", affiliation: `Associate Professor, University of Calabria.\nAgenda: Minds, Morals, and Mimicry: Probing Language Models’ Human-Like Affective Abilities`, imagePath: "./keynoteSpeakers/andrea.jpeg", link: "https://www.linkedin.com/in/andrea-tagarelli/?locale=en_US" }
 ];
  
